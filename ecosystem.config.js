9c979b5b92f89b394894fe746a2a147806500a3e
Abhsihar Jangir <abhishar.jangir@abspl.in>
2018-02-13 15:36:05 +0530

    Remove SSR

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   apps: [
     {
       name: 'REACTOR',
-      script: 'server/index.js',
+      script: 'index.js',
       log_date_format: 'YYYY-MM-DD HH:mm Z',
       combine_logs: true,
       env: {
